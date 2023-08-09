c568a59f1cc1802bbdaf364a969446e412ef64f1
romakarimov <ramizkarimov@gmail.com>
2023-08-09 22:08:38 +0500

    кнобкани ишлаябди

--- app/src/main/java/com/example/mygid/MainActivity.java
+++ app/src/main/java/com/example/mygid/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mygid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
+    void initViews(){
+        Button nav = (Button) findViewById(R.id.nav);
+        nav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opennav();
+            }
+        });
+    }
+
+    void opennav(){
+        Intent intent= new Intent(this, ActivityNav.class);
+        startActivity(intent);
+    }
+
 }
+
